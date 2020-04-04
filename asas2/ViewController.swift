6db50176a4bb4d23303892f921a996af76a1d1e8
Saj Panchal <sajpanchal@gmail.com>
2020-04-04 13:11:41 -0400

    Initial Commit

---
+++ asas2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  asas2
+//
+//  Created by saj panchal on 2020-04-04.
+//  Copyright © 2020 saj panchal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ asas2Tests/asas2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  asas2Tests.swift
+//  asas2Tests
+//
+//  Created by saj panchal on 2020-04-04.
+//  Copyright © 2020 saj panchal. All rights reserved.
+//
+
+import XCTest
+@testable import asas2
+
+class asas2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
