e972ccb78e876dac3bc5ecc643f8bfc4fc0f482a
Saj Panchal <sajpanchal@gmail.com>
2021-04-25 17:48:27 -0400

    folders renamed

--- MyForecastTests/MyForecastTests.swift
+++ MyForecastTests/MyForecastTests.swift
@@ -7,9 +7,9 @@
 //
 
 import XCTest
-@testable import asas2
+@testable import MyForecast
 
-class asas2Tests: XCTestCase {
+class MyForecastTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- MyForecastUITests/MyForecastUITests.swift
+++ MyForecastUITests/MyForecastUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class asas2UITests: XCTestCase {
+class MyforecastUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
